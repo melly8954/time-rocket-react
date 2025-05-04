c51b53c710451714405f04a2fd3f35417d0a2087
haily <49448330+melly8954@users.noreply.github.com>
2025-05-04 17:48:28 +0900

    250504 로그인 구현 ver_0.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/common/Header'
 import Home from './pages/Home';
 import SignUp from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/rocket" element={<div>로켓 제작 페이지</div>} />
         <Route path="/display" element={<div>진열장 페이지</div>} />
         <Route path="/chest" element={<div>보관함 페이지</div>} />
---
+++ src/authStore.js
@@ -0,0 +1,12 @@
+import { create } from 'zustand';
+
+const useAuthStore = create((set) => ({
+  isLoggedIn: false, // 로그인 상태
+  accessToken: null, // accessToken 저장
+  rememberMe: false, // 로그인 유지 여부
+  setIsLoggedIn: (status) => set({ isLoggedIn: status }), // 로그인 상태 설정
+  setAccessToken: (token) => set({ accessToken: token }), // accessToken 설정
+  setRememberMe: (status) => set({ rememberMe: status }), // 로그인 상태 유지 설정
+}));
+
+export default useAuthStore;
