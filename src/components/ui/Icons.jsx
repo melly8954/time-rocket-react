8e5a4ae4be9cb88394c16d4a89997cdac16ae6ed
hyunbi123 <junghyunbi512@naver.com>
2025-06-05 07:53:46 +0900

    250605 GroupRocket Update2

--- src/components/ui/Icons.jsx
+++ src/components/ui/Icons.jsx
@@ -583,3 +583,9 @@ export const ChatIcon = ({ className }) => (
     <path fillRule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clipRule="evenodd" />
   </svg>
 );
+
+export const KickIcon = ({ className }) => (
+  <svg className={className} fill="currentColor" viewBox="0 0 20 20">
+    <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+  </svg>
+);
