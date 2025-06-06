db1f0a61342078344ae7375ac3f4efb390718804
haily <49448330+melly8954@users.noreply.github.com>
2025-06-06 21:00:51 +0900

    실시간 채팅 테스트

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   define: {
+    global: 'window',  // global을 window로 매핑
+  },
   plugins: [
     react({
       jsxImportSource: 'react',
